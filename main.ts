41f1a5a319e1c34a5b71bacfa85124afa38e3809
Ishaan Pradhan <81606798+Ishaan777777@users.noreply.github.com>
2021-10-28 17:21:52 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,22 @@
 input.onButtonPressed(Button.A, function () {
-    if (pins.analogReadPin(AnalogPin.P0) <= LCal - 30) {
-        MotorDriver.MotorRun(Motor.Left, Dir.backward, 16)
-        MotorDriver.MotorRun(Motor.Right, Dir.forward, 10)
+    if (pins.analogReadPin(AnalogPin.P0) <= Left - 30) {
+        MotorDriver.MotorRun(Motor.Left, Dir.forward, 16)
+        MotorDriver.MotorRun(Motor.Right, Dir.forward, 7)
         motobit.setMotorSpeed(Motor.Left, MotorDirection.Reverse, 75)
         motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 25)
         basic.pause(50)
-    } else if (pins.analogReadPin(AnalogPin.P2) <= RCal - 30) {
-        MotorDriver.MotorRun(Motor.Right, Dir.backward, 16)
-        MotorDriver.MotorRun(Motor.Left, Dir.backward, 16)
+    } else if (pins.analogReadPin(AnalogPin.P2) <= Right - 30) {
+        MotorDriver.MotorRun(Motor.Right, Dir.forward, 16)
+        MotorDriver.MotorRun(Motor.Left, Dir.forward, 7)
         motobit.setMotorSpeed(Motor.Right, MotorDirection.Reverse, 75)
         motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 25)
         basic.pause(50)
     } else {
-        motobit.setMotorSpeed(Motor.Left, MotorDirection.Reverse, 40)
-        motobit.setMotorSpeed(Motor.Right, MotorDirection.Reverse, 40)
+        MotorDriver.MotorRun(Motor.Left, Dir.forward, 10)
+        MotorDriver.MotorRun(Motor.Right, Dir.forward, 10)
     }
 })
-let RCal = 0
-let LCal = 0
-LCal = pins.analogReadPin(AnalogPin.P0)
-RCal = pins.analogReadPin(AnalogPin.P2)
+let Right = 0
+let Left = 0
+Left = pins.analogReadPin(AnalogPin.P0)
+Right = pins.analogReadPin(AnalogPin.P2)
